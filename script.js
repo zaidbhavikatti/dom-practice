6d9336e4a6b41ed28b8809e64b48a7a16b47ef59
zaidbhavikatti <98631203+zaidbhavikatti@users.noreply.github.com>
2022-04-23 12:51:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let ner=document.getElementsByClassName("para");
+console.log(ner);
+document.p="we change the paragraph";
+document.getElementById("hOne").innerText=23;
+
+let ger=document.querySelector(p);
+console.log(ger);
+
+let der=document.querySelectorAll(".p");
+console.log("der");
+
+let yep=document.getElementsByName("h3");
+console.log("yep");
